3f18c15fee275f9c046b856cd06bbf869a96c454
Yanhao Li <hi@yanhaoli.com>
2020-09-24 18:30:27 -0400

    feat: convext hull

--- src/hull/main.cpp
+++ src/hull/main.cpp
@@ -9,40 +9,58 @@
 #include <math.h>
 ////////////////////////////////////////////////////////////////////////////////
 
-#define PI 3.14159265
 typedef std::complex<double> Point;
 typedef std::vector<Point> Polygon;
 
+// the determinant of u and v
 double inline det(const Point &u, const Point &v) {
-	// TODO
-	return 0;
+	return u.real()*v.imag() - v.real()*u.imag();
 } 
 
-double inline dot(const Point &u, const Point &v) {
-	return u.real() * v.real() + u.imag() * v.imag();
+bool inline salientAngle(const Point &a, const Point &b, const Point &c) {
+	// a => first point
+	// b => mid point
+	// c => new point
+	Point ac(c.real()-a.real(), c.imag()-a.imag());
+	Point ab(b.real()-a.real(), b.imag()-a.imag());
+
+	return det(ab, ac) > 0;
 }
 
-double inline getAngle(const Point p0, const Point p) {
-	double y = abs(p.imag() - p0.imag());
-	double x = abs(p.real() - p0.real());
-	double angle = p0 == p ? 0 : atan(y / x);
-	angle = angle < 0 ? angle + PI : angle;
-	return angle;
+// get the distance between two points
+double inline dist(const Point &p1, const Point &p2) {
+	return sqrt(pow(p2.real() - p1.real(), 2) + pow(p2.imag() - p1.imag(), 2)); 
 }
 
 struct Compare {
-	Point p0; // Leftmost point of the poly
-	bool operator ()(const Point p1, const Point p2) {
-		double angle1 = getAngle(p0, p1);
-		double angle2 = getAngle(p0, p2);
-		return angle1 < angle2;
+	Point p0; // lowest point of the poly
+	bool operator ()(const Point &p1, const Point &p2) {
+		Point p0p1(p1.real() - p0.real(), p1.imag() - p0.imag());
+		Point p0p2(p2.real() - p0.real(), p2.imag() - p0.imag());
+		double d = det(p0p1, p0p2);
+		if (d == 0) {
+			return dist(p0, p1) < dist(p0, p2);
+		}
+
+		return d > 0;
 	}
 };
 
-bool inline salientAngle(Point &a, Point &b, Point &c) {
-	std::complex<double> ba(a.real() - b.real(), a.imag() - b.imag());
-	std::complex<double> bc(c.real() - b.real(), c.imag() - b.imag());
-	return dot(ba, bc) > 0;
+
+std::vector<Point> load_xyz(const std::string &filename) {
+	std::vector<Point> points;
+	std::ifstream in(filename);
+
+	int total_points;
+	in >> total_points;
+
+	double x, y, z;
+	while (in >> x >> y >> z)
+	{
+		points.push_back(Point(x, y));
+	}
+
+	return points;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -52,9 +70,9 @@ Polygon convex_hull(std::vector<Point> &points) {
 	// find p0
 	Point p0 = points[0];
 	for (Point point : points) {
-		if (point.real() < p0.real()) {
+		if (point.imag() < p0.imag()) {
 			p0 = point;
-		} else if (point.real() == p0.real() && point.imag() < p0.imag()) {
+		} else if (point.imag() == p0.imag() && point.real() < p0.real()) {
 			p0 = point;
 		}
 	}
@@ -62,40 +80,34 @@ Polygon convex_hull(std::vector<Point> &points) {
 	order.p0 = p0;
 	std::sort(points.begin(), points.end(), order);
 
-	Polygon hull;
-	for (int i = 0; i < points.size(); i++) {
-		const Point point = points[i];
+	// std::vector<Point> temp = points;
+	// points.clear();
 
-		if (i > 1 && salientAngle(points[i], points[i - 1], points[i - 2])) {
+	// If several points have same polar angle, only keep the farthest one
+	// points.push_back(temp[0]);
+	// for (int i = 1; i < temp.size(); i++) {
+	// 	while (i < temp.size() - 1 && getPolarAngle(p0, temp[i]) == getPolarAngle(p0, temp[i + 1])) {
+	// 		i++;
+	// 	}
+	// 	points.push_back(temp[i]);
+	// }
+
+	Polygon hull;
+	hull.push_back(points[0]);
+	hull.push_back(points[1]);
+	hull.push_back(points[2]);
+
+	for (int i = 3; i < points.size(); i++) {
+		while (!salientAngle(*(hull.rbegin() + 1), hull.back(), points[i])) {
 			hull.pop_back();
 		}
 
 		hull.push_back(points[i]);
 	}
-	// use salientAngle(a, b, c) here
 	return hull;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-
-std::vector<Point> load_xyz(const std::string &filename) {
-	std::vector<Point> points;
-	std::ifstream in(filename);
-
-	std::string line;
-
-	std::getline(in, line);
-	int total_points = stoi(line);
-
-	double x, y;
-	while (std::getline(in, line))
-	{
-		in >> x >> y;
-		points.push_back(Point(x, y));
-	}
-
-	return points;
-}
 
 void save_obj(const std::string &filename, Polygon &poly) {
 	std::ofstream out(filename);
@@ -106,9 +118,9 @@ void save_obj(const std::string &filename, Polygon &poly) {
 	for (const auto &v : poly) {
 		out << "v " << v.real() << ' ' << v.imag() << " 0\n";
 	}
-	for (size_t i = 0; i < poly.size(); ++i) {
-		out << "l " << i+1 << ' ' << 1+(i+1)%poly.size() << "\n";
-	}
+	// for (size_t i = 0; i < poly.size(); ++i) {
+	// 	out << "l " << i+1 << ' ' << 1+(i+1)%poly.size() << "\n";
+	// }
 	out << std::endl;
 }
 
